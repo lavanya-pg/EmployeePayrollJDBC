85a3edf9ca01839a8db4b4152e9b378cce2c2395
lavanya-pg <lavanyapg8@gmail.com>
2021-08-16 21:49:03 +0530

    Added: Ability to update the salary of Basepay for Employee

--- lib/src/main/java/employeepayrolljdbc/EmployeePayrollJDBC.java
+++ lib/src/main/java/employeepayrolljdbc/EmployeePayrollJDBC.java
@@ -1,41 +1,41 @@
 package employeepayrolljdbc;
 
 import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Enumeration;
+	import java.sql.Driver;
+	import java.sql.DriverManager;
+	import java.util.Enumeration;
 
+	public class EmployeePayrollJDBC {
 
-public class EmployeePayrollJDBC
-{
-	public static void main(String[] args)
-	{
-		try
-		{
-			Class.forName("com.mysql.jdbc.Driver");
-		} 
-		catch (ClassNotFoundException e)
-		{
-			e.printStackTrace();
+		public static void main(String[] args) {
+
+			String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+			String userName = "root";
+			String password = "Lavanya*871999";
+			Connection connection;
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				System.out.println("Driver loaded!");
+			} catch (ClassNotFoundException e) {
+				throw new IllegalStateException("Cannot find the driver in the classpath");
+			}
+			listDrivers();
+			try {
+				System.out.println("connecting to the database:" + jdbcURL);
+				connection = DriverManager.getConnection(jdbcURL, userName, password);
+				System.out.println("connection is successful!!!!" + connection);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
-		ListDrivers();
-		try {
-			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306//payroll_service?useSSL=false"
-				, "root", "Lavanya*871999");
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		System.out.println("Connection Done..!!!");
-	}
 
-	private static void ListDrivers() {
-		Enumeration<Driver> driverList=DriverManager.getDrivers();
-		while(driverList.hasMoreElements()) {
-			Driver driverClass=(Driver)driverList.nextElement();
-			System.out.println("  "+driverClass.getClass().getName());
+		private static void listDrivers() {
+			Enumeration<Driver> driverList = DriverManager.getDrivers();
+			while (driverList.hasMoreElements()) {
+				Driver driverClass = driverList.nextElement();
+				System.out.println(driverClass.getClass().getName());
+			}
 		}
 	}
 	
 
-}
