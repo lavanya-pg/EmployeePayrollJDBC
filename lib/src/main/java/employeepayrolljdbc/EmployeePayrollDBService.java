36e1d67488e5c2a7af9169dcea757319d14a84f1
lavanya-pg <lavanyapg8@gmail.com>
2021-08-16 22:30:05 +0530

    Added: Ability to Retrive all Employees who have Joined in a Particular Data Range from the payroll service Database

--- lib/src/main/java/employeepayrolljdbc/EmployeePayrollDBService.java
+++ lib/src/main/java/employeepayrolljdbc/EmployeePayrollDBService.java
@@ -26,7 +26,7 @@ public class EmployeePayrollDBService {
     private Connection getConnection() throws SQLException {
         String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
         String userName = "root";
-        String password = "maya";
+        String password = "Lavanya*871999";
         Connection connection;
         System.out.println("Connectinng to database:" + jdbcURL);
         connection = DriverManager.getConnection(jdbcURL, userName, password);
